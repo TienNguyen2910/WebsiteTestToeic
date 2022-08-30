870c113ce1b57925ec8fa7a8a18c3067d6f4e67c
TienNguyen2910 <tiennguyenthimy29@gmail.com>
2022-08-30 23:34:25 +0700

    project initialization

---
+++ WebsiteTestToeic.Domain/Models/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace WebsiteTestToeic.Domain.Models
+{
+    public class Role
+    {
+        public Role()
+        {
+            Users = new List<User>();
+        }
+        public int Id { get; set; }
+        public string? RoleName { get; set; }
+        
+        [JsonIgnore]
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ WebsiteTestToeic.Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteTestToeic.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }    
+        public DateOnly DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+    }
+}
