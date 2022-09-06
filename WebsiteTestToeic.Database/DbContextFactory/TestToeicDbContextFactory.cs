0b0e17347e4e2e94204ae04918de20f3473e2fcc
TienNguyen2910 <tiennguyenthimy29@gmail.com>
2022-09-06 21:48:51 +0700

    create dbcontext Factory

---
+++ WebsiteTestToeic.Database/DbContextFactory/TestToeicDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿
+using WebsiteTestToeic.Database.DatabaseContext;
+
+namespace WebsiteTestToeic.Database.DbContextFactory
+{
+    public class TestToeicDbContextFactory
+    {
+        private static TestToeicDbContext _testToeicDbContext = null;
+        public static TestToeicDbContext GetDbContext()
+        {
+            if(_testToeicDbContext == null)
+                _testToeicDbContext = new TestToeicDbContext();
+            return _testToeicDbContext;
+        }
+    }
+}
