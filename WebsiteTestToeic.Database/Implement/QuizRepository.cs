53f08f3ec2b3b1a3c2fed06b7b8e2cce08d5ea8e
TienNguyen2910 <tiennguyenthimy29@gmail.com>
2022-10-02 23:01:12 +0700

    write repository Quiz

---
+++ WebsiteTestToeic.Database/Implement/QuizRepository.cs
@@ -0,0 +1,71 @@
+﻿using WebsiteTestToeic.Database.DatabaseContext;
+using WebsiteTestToeic.Database.DbContextFactory;
+using WebsiteTestToeic.Database.Interface;
+using WebsiteTestToeic.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebsiteTestToeic.Database.Implement
+{
+    public class QuizRepository : IQuizRepository
+    {
+        private TestToeicDbContext _context;
+        public QuizRepository(TestToeicDbContext context = null)
+        {
+            if (context == null)
+                _context = TestToeicDbContextFactory.GetDbContext();
+            else _context = context;
+        }
+        public async Task<Quiz> AddQuiz(Quiz quiz)
+        {
+            Quiz q = new Quiz()
+            {
+                Title = quiz.Title,
+                TestId = quiz.TestId,
+                ActorId = quiz.ActorId
+            };
+            await _context.Quizzes.AddAsync(q);
+            await _context.SaveChangesAsync();
+            return q;
+        }
+
+        public async Task<bool> DeleteQuiz(int id)
+        {
+            bool result = false;
+            Quiz quiz = await _context.Quizzes.FirstOrDefaultAsync(q => q.Id == id);
+            if(quiz != null)
+            {
+                _context.Quizzes.Remove(quiz);
+                await _context.SaveChangesAsync();
+                result = true;
+            }
+            return quiz;
+        }
+
+        public async Task<List<Quiz>> GetAllQuizs()
+        {
+            List<Quiz> listQuiz = await _context.Quizzes.ToListAsync();
+            return listQuiz;
+        }
+
+        public async Task<Quiz> GetQuiz(int id)
+        {
+            Quiz quiz = await _context.Quizzes.FirstOrDefaultAsync(q => q.Id == id);
+            if(quiz != null)
+                return quiz;
+            return null;
+        }
+
+        public async Task<Quiz> UpdateQuiz(Quiz quiz)
+        {
+            Quiz q = await _context.Quizzes.FindAsync(quiz.Id);
+            f(quiz != null){
+                q.Title = quiz.Title;
+                q.TestId = quiz.TestId;
+                q.ActorId = quiz.ActorId;
+                await _context.SaveChangesAsync();
+                return q;
+            }
+            return null;
+        }
+    }
+}
---
+++ WebsiteTestToeic.Database/Interface/IQuizRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebsiteTestToeic.Domain.Models;
+
+namespace WebsiteTestToeic.Database.Interface
+{
+    public interface IQuizRepository
+    {
+        public Task<List<Quiz>> GetAllQuizs();
+        public Task<Quiz> GetQuiz(int id);
+        public Task<Quiz> AddQuiz(Quiz quiz);
+        public Task<Quiz> UpdateQuiz(Quiz quiz);
+        public Task<bool> DeleteQuiz(int id);
+    }
+}
