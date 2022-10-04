0536295fcab09f34b002c2ea9eba254d397464a3
TienNguyen2910 <tiennguyenthimy29@gmail.com>
2022-10-04 07:24:37 +0700

    write controller Quiz

---
+++ WebsiteTestToeic.Api/Controller/QuizController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteTestToeic.Database.Interface;
+using WebsiteTestToeic.Domain.Models;
+
+namespace WebsiteTestToeic.Api.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizController : ControllerBase
+    {
+        private readonly IQuizRepository _quizrepository;
+        public QuizController(IQuizRepository quizRepository)
+        {
+            _quizrepository = quizRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Quiz>> GetAllQuizs()
+        {             
+            return Ok(await _quizrepository.GetAllQuizs());
+        }
+        [HttpGet("id:int")]
+        public  async Task<ActionResult<Quiz>> GetQuizById(int id)
+        {
+            return Ok(await _quizrepository.GetQuiz(id));
+        }
+        [HttpPost]
+        public async Task<ActionResult<Quiz>> AddQuiz(Quiz quiz)
+        {
+        return Ok(await _quizrepository.AddQuiz(quiz));
+        }
+    }
+}
