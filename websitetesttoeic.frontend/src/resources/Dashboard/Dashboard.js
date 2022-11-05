1eb666db255a4ff7a96a30f361a617320b07eed5
Tran Quoc Bao <baob1809438@student.ctu.edu.vn>
2022-11-05 22:28:01 +0700

    Fix bảng xếp hạng

--- websitetesttoeic.frontend/src/resources/Dashboard/Dashboard.js
+++ websitetesttoeic.frontend/src/resources/Dashboard/Dashboard.js
@@ -39,7 +39,7 @@ function Dashboard() {
     return (
         <div className="row d-flex justify-content-center">
             <div className="col-8">
-                <div className="px-4 shadow my-4 rating">
+                <div className="p-4 pt-0 shadow my-4 rating">
                     <div className="row mb-3">
                         {params.idTest === "1" ? (
                             <h4 className="mt-2 p-0">Full Test</h4>
@@ -57,33 +57,47 @@ function Dashboard() {
                 </div>
             </div>
             <div className="col-4 shadow-5 my-4 rating">
-                <img
-                    className="mt-2"
-                    src={`${REACT_APP_CLIENT}/image_rating.png`}
-                    alt="Image rating"
-                    height={60}
-                    loading="lazy"
-                />
-                <h4 className="d-inline px-3 mt-2">Bảng xếp hạng</h4>
-                <section className="py-5">
-                    <ul className="timeline-with-icons">
-                        {listResult.map((result, index) => (
+                <div className="mx-2 align-middle">
+                    <img
+                        className="mt-2"
+                        src={`${REACT_APP_CLIENT}/image_rating.png`}
+                        alt="Image rating"
+                        height={60}
+                        loading="lazy"
+                    />
+                    <h4 className="d-inline px-3 mt-2">Bảng xếp hạng</h4>
+                </div>
+                <section className="pt-5">
+                    <ul className="timeline-with-icons mx-4">
+                        {listResult.slice(0, 10).map((result, index) => (
                             <li className="timeline-item mb-5" key={index}>
-                                <span className={`timeline-icon text-white ${index === 0 ? "bg-danger" : ""}`}>
+                                <span
+                                    className={`timeline-icon text-white ${
+                                        index === 0
+                                            ? "bg-danger"
+                                            : index === 1
+                                            ? "bg-warning"
+                                            : index === 2
+                                            ? "bg-success"
+                                            : ""
+                                    }`}
+                                >
                                     {index + 1}
                                 </span>
-                                <div className="item-left d-inline">
-                                    <img
-                                        src={`${REACT_APP_CLIENT}/default.jpg`}
-                                        className="rounded-circle"
-                                        height={35}
-                                        alt="Portrait of a Woman"
-                                        loading="lazy"
-                                    />
-                                    <h5 className="d-inline px-2 small">{result.user.userName}</h5>
-                                </div>
-                                <div className="item-right d-inline fw-bold">
-                                    <span>{result.score}</span>
+                                <div className="row">
+                                    <div className="col-6">
+                                        <img
+                                            src={`${REACT_APP_CLIENT}/default.jpg`}
+                                            className="rounded-circle"
+                                            height={35}
+                                            alt="Portrait of a Woman"
+                                            loading="lazy"
+                                        />
+                                        <h5 className="d-inline px-2 small">{result.user.userName}</h5>
+                                    </div>
+                                    <div className="col-6 fw-bold text-right">
+                                        <p>{result.score}</p>
+                                    </div>
                                 </div>
                             </li>
                         ))}
